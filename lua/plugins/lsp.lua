abca935df827224a5f7bdc0c1733047cc4044724
KJCats <lultews@gmail.com>
2025-08-20 02:25:50 +0200

    Added lua/plugins/lsp.lua with rust-analizer in focus

---
+++ lua/plugins/lsp.lua
@@ -0,0 +1,39 @@
+return {
+  -- Mason (LSP/Debugger/Formatter/Linter Manager)
+  {
+    "williamboman/mason.nvim",
+    config = function()
+      require("mason").setup()
+    end,
+  },
+
+  -- Mason-LSP
+  {
+    "williamboman/mason-lspconfig.nvim",
+    config = function()
+      require("mason-lspconfig").setup({
+        ensure_installed = { "lua_ls", "pyright" }, -- Automatisch installierte LSPs
+      })
+    end,
+  },
+
+  -- nvim-lspconfig
+  {
+    "neovim/nvim-lspconfig",
+    config = function()
+      local lspconfig = require("lspconfig")
+      lspconfig.lua_ls.setup({}) -- LSP for Lua
+
+      -- LSP for Rust
+      lspconfig.rust_analyzer.setup({
+        settings = {
+          ["rust-analyzer"] = {
+            checkOnSave = {
+              command = "clippy",
+            },
+          },
+        },
+      })
+    end,
+  },
+}
